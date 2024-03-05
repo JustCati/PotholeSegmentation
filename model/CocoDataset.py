cc950f79378547d00f4891a734f856a9b0519cdd
JustCati <ale.catinello@gmail.com>
2024-03-05 12:51:22 +0100

    feat: MaskDataset for U-Net

--- model/CocoDataset.py
+++ model/CocoDataset.py
@@ -9,6 +9,51 @@ from torchvision import transforms
 from pycocotools.coco import COCO
 
 
+class MaskDataset(data.Dataset):
+    def __init__(self, path, annFile):
+        self.root = path
+        self.coco = COCO(annFile)
+        self.ids = list(self.coco.imgs.keys())
+
+    def _combineMasks(self, data):
+        ret = torch.cat(data, dim=0)
+        ret = torch.sum(ret, dim=0)
+        ret = (ret > 0).float()
+        return ret
+
+    def __getitem__(self, index):
+        coco = self.coco
+        img_id = self.ids[index]
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        target = coco.loadAnns(ann_ids)
+
+        #* Load the image
+        path = coco.loadImgs(img_id)[0]['file_name']
+        img = Image.open(os.path.join(self.root, path)).convert('RGB')
+
+        transformer = transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize(np.mean(img, axis=(0, 1)), np.std(img, axis=(0, 1)))
+        ])
+        img = transformer(img)
+
+        #* Load Masks
+        masks = []
+        nums = len(target)
+        elem = [coco.annToMask(target[i]) for i in range(nums)]
+        for i in range(nums):
+            masks.append(transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Resize((256, 256)),
+                transforms.ToTensor()
+            ])(elem[i]))
+        return img, torch.as_tensor(self._combineMasks(masks))
+
+    def __len__(self):
+        return len(self.ids)
+
+
 
 class CocoDataset(data.Dataset):
     def __init__(self, path, annFile):
