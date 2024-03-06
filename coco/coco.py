69d16c0e46e74e487ac4bbca4655a84616417357
SO4REN <ale.catinello@gmail.com>
2024-03-06 12:24:00 +0100

    fix: split name

--- coco/coco.py
+++ coco/coco.py
@@ -95,7 +95,7 @@ def generateJSON(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Script for conversion of YoloV8 labels to COCO format.')
     parser.add_argument("--path", type=str, default=os.path.join(os.getcwd(), "data"), help="Path to the data directory")
-    parser.add_argument("--split", type=str, default="train", help="Target directory (train, test, val)", choices=["train", "test", "val"])
+    parser.add_argument("--split", type=str, default="train", help="Target directory (train, test, validation)", choices=["train", "test", "validation"])
     parser.add_argument("--target", type=str, default="images", help="Target directory (images, videos)", choices=["images", "videos"])
     args = parser.parse_args()
 
