0956c202b6662875ecb314aad7ce30ba0bf58bba
JustCati <ale.catinello@gmail.com>
2024-03-21 10:27:15 +0100

    feat: GaussianBlur with percentage of happening

--- utils/transform.py
+++ utils/transform.py
@@ -1,5 +1,33 @@
 import torch
 import numpy as np
+from torchvision.transforms import v2 as T
+
+
+
+class GaussianBlur(object):
+    def __init__(self, p = 0.5, kernel_size = 5, sigma = 1):
+        self.p = p
+        self.kernel_size = kernel_size
+        self.sigma = sigma
+
+    def __call__(self, img, target):
+        if torch.rand(1) < self.p:
+            if not isinstance(img, torch.Tensor):
+                raise ValueError(f"Expected torch.Tensor, got {type(img)}")
+
+            dtype = img.dtype
+            if not img.is_floating_point():
+                img = img.to(torch.float32)
+
+            kernel = T.GaussianBlur(self.kernel_size, sigma = self.sigma)
+            out = kernel(img)
+            if out.dtype != dtype:
+                out = out.to(dtype)
+            return out, target
+        return img, target
+
+    def __repr__(self):
+        return self.__class__.__name__
 
 
 
