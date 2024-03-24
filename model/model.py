88cfc89e5c24d4329c34500aacacdcd9f7e685d1
JustCati <ale.catinello@gmail.com>
2024-03-24 11:50:36 +0100

    refactor: evalueate single epoch function

--- model/model.py
+++ model/model.py
@@ -78,45 +78,34 @@ def train_one_epoch(model, loader, optimizer, lr_scheduler, device):
 
 
 
+def evaluate_one_epoch(model, valLoader, MASK_THRESHOLD, device):
+    total_val = []
+    with torch.no_grad():
+        for images, targets in valLoader:
+            images = list([image.to(device) for image in images])
+            targets = [{k: v.to(device) for k, v in elem.items()} for elem in targets]
+            imgSize_X = images[0].shape[-1]
+            imgSize_Y = images[0].shape[-2]
 
-def trainModel(model, trainLoader, valLoader, optimizer, lr_scheduler, n_epoch, path = os.getcwd(), device = torch.device("cpu")):
-    val_accuracy = {}
-    train_losses = {}
-    best_Acc = float("-inf")
+            pred = model(images)
+            pred = [{k: (v > MASK_THRESHOLD).reshape(-1 , imgSize_X, imgSize_Y) if k == "masks" else v for k, v in elem.items()} for elem in pred]
+            targets = [{k: (v > MASK_THRESHOLD).reshape(-1 , imgSize_X, imgSize_Y) if k == "masks" else v for k, v in elem.items()} for elem in targets]
 
-    for epoch in range(n_epoch):
+            map_segm = MAP(box_format="xyxy", iou_type="segm")
+            map_bbox = MAP(box_format="xyxy", iou_type="bbox")
 
-        #* --------------- Train the model ----------------
-        model.train()
-        train_loss = train_one_epoch(model, trainLoader, optimizer, lr_scheduler, device)
-        train_losses.update({int(epoch): train_loss})
+            map_segm.update(pred, targets)
+            map_bbox.update(pred, targets)
 
-        #* --------------- Evaluate the model -------------
-        total_val = []
-        model.eval()
-        with torch.no_grad():
-            for images, targets in valLoader:
-                images = list([image.to(device) for image in images])
-                targets = [{k: v.to(device) for k, v in elem.items()} for elem in targets]
+            res_segm = {"segm_" + k: v.item() for k, v in map_segm.compute().items()}
+            res_bbox = {"bbox_" + k: v.item() for k, v in map_bbox.compute().items()}
 
-                pred = model(images)
-                pred = [{k: (v > 0).reshape(-1 , 640, 640) if k == "masks" else v for k, v in elem.items()} for elem in pred]
-                targets = [{k: (v > 0).reshape(-1 , 640, 640) if k == "masks" else v for k, v in elem.items()} for elem in targets]
+            val_acc = {}
+            val_acc.update(res_segm)
+            val_acc.update(res_bbox)
 
-                map_segm = MAP(box_format="xyxy", iou_type="segm")
-                map_bbox = MAP(box_format="xyxy", iou_type="bbox")
-
-                map_segm.update(pred, targets)
-                map_bbox.update(pred, targets)
-
-                res_segm = {"segm_" + k: v.item() for k, v in map_segm.compute().items()}
-                res_bbox = {"bbox_" + k: v.item() for k, v in map_bbox.compute().items()}
-
-                val_acc = {}
-                val_acc.update(res_segm)
-                val_acc.update(res_bbox)
-
-                total_val.append(val_acc)
+            total_val.append(val_acc)
+    return {k: sum(acc[k] for acc in total_val) / len(total_val) for k in total_val[0]}
         #* ------------------------------------------------
 
         val_accuracy.update({int(epoch): {k: sum(acc[k] for acc in total_val) / len(total_val) for k in total_val[0]}})
